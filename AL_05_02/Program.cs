de5dfe9296e23ba8ca49c82d8ca6f7b9abaab780
Jakub Kliś <jakubklis2201@gmail.com>
2024-05-06 11:12:42 +0200

    Gotowe

---
+++ AL_05_02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            string graphType = Console.ReadLine().Trim();
+            int n = int.Parse(Console.ReadLine());
+            List<string> edges = new List<string>();
+            
+            for (int j = 0; j < n; j++)
+            {
+                string edge = Console.ReadLine();
+                edges.Add(edge);
+            }
+
+            switch (graphType)
+            {
+                case "g":
+                    Console.WriteLine("graph {");
+                    foreach (var edge in edges)
+                    {
+                        var nodes = edge.Split();
+                        Console.WriteLine($"{nodes[0]} -- {nodes[1]};");
+                    }
+                    Console.WriteLine("}");
+                    break;
+                case "d":
+                    Console.WriteLine("digraph {");
+                    foreach (var edge in edges)
+                    {
+                        var nodes = edge.Split();
+                        Console.WriteLine($"{nodes[0]} -> {nodes[1]};");
+                    }
+                    Console.WriteLine("}");
+                    break;
+                case "gw":
+                    Console.WriteLine("graph {");
+                    foreach (var edge in edges)
+                    {
+                        var parts = edge.Split();
+                        Console.WriteLine($"{parts[0]} -- {parts[1]} [label = {parts[2]}];");
+                    }
+                    Console.WriteLine("}");
+                    break;
+                case "dw":
+                    Console.WriteLine("digraph {");
+                    foreach (var edge in edges)
+                    {
+                        var parts = edge.Split();
+                        Console.WriteLine($"{parts[0]} -> {parts[1]} [label = {parts[2]}];");
+                    }
+                    Console.WriteLine("}");
+                    break;
+            }
+        }
+    }
+}
---
+++ PT07Z/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LongestPathInTree
+{
+    static List<int>[] graph;
+    static bool[] visited;
+    static int farthestNode = 0;
+    static int maxDistance = 0;
+
+    public static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        graph = new List<int>[N + 1];
+
+        for (int i = 1; i <= N; i++)
+        {
+            graph[i] = new List<int>();
+        }
+
+        for (int i = 1; i < N; i++)
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            int u = int.Parse(tokens[0]);
+            int v = int.Parse(tokens[1]);
+
+            graph[u].Add(v);
+            graph[v].Add(u);
+        }
+
+        visited = new bool[N + 1];
+        BFS(1);
+
+        visited = new bool[N + 1];
+        BFS(farthestNode);
+
+        Console.WriteLine(maxDistance);
+    }
+
+    static void BFS(int startNode)
+    {
+        Queue<(int node, int distance)> queue = new Queue<(int, int)>();
+        queue.Enqueue((startNode, 0));
+        visited[startNode] = true;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            int currentNode = current.node;
+            int currentDistance = current.distance;
+
+            if (currentDistance > maxDistance)
+            {
+                maxDistance = currentDistance;
+                farthestNode = currentNode;
+            }
+
+            foreach (int neighbor in graph[currentNode])
+            {
+                if (!visited[neighbor])
+                {
+                    visited[neighbor] = true;
+                    queue.Enqueue((neighbor, currentDistance + 1));
+                }
+            }
+        }
+    }
+}
